9aba6b5eeb07987b8c4c3505ba354dfab09651cc
Hackbright-student <info@hackbrightacademy.com>
2016-01-06 12:07:59 -0800

    Added the arithmetic

--- arithmetic.py
+++ arithmetic.py
@@ -1,3 +1,14 @@
+def verify_for_2_numbers(num1, num2):
+    """Verifies that both numbers are ints"""
+    try:
+        if num1 is number and num2 is number:
+            
+
+
+    except ValueError:
+        print "Please type in a number!"
+        
+
 def add(num1, num2):
     return (num1 + num2)
 
@@ -8,7 +19,7 @@ def multiply(num1, num2):
     return (num1 * num2)
 
 def divide(num1, num2):
-    return (num1 / num2)
+    return (float(num1) / float(num2))
 
 def square(num1):
     return (num1 * num1)
